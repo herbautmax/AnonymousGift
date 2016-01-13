13c2b08570ce1fd52204a0c0a8e117c7ed069e22
Maxime Herbaut <maximeherbaut@MacBook-Pro-de-Maxime.local>
2016-01-13 15:33:19 +0100

    add attendee

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -7,97 +7,112 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\Event;
+use AppBundle\Entity\Email;
 use AppBundle\Form\EventType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
- * Event controller.
- *
- * @Route("/event")
- */
+* Event controller.
+*
+* @Route("/event")
+*/
 class EventController extends Controller
 {
-    /**
-     * Lists all Event entities.
-     *
-     * @Route("/", name="event_index")
-     * @Method("GET")
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
 
-        $events = $em->getRepository('AppBundle:Event')->findAll();
+  /**
+  * Creates a new Event entity.
+  *
+  * @Route("/new", name="event_new")
+  * @Method({"GET", "POST"})
+  */
+  public function newAction(Request $request)
+  {
+    $event = new Event();
+    $form = $this->createForm('AppBundle\Form\EventType', $event);
+    $form->handleRequest($request);
 
-        return $this->render('event/index.html.twig', array(
-            'events' => $events,
-        ));
+    if ($form->isSubmitted() && $form->isValid()) {
+
+      $owner = $this->getUser();
+      $event->setOwner($owner);
+
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($event);
+      $em->flush();
+
+      return $this->redirectToRoute('perso');
     }
 
-    /**
-     * Creates a new Event entity.
-     *
-     * @Route("/new", name="event_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $event = new Event();
-        $form = $this->createForm('AppBundle\Form\EventType', $event);
-        $form->handleRequest($request);
+    return $this->render('event/new.html.twig', array(
+      'event' => $event,
+      'form' => $form->createView(),
+    ));
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
+  /**
+  * Finds and displays a Event entity.
+  *
+  * @Route("/{id}", name="event_show")
+  * @Method({"GET", "POST"})
+  */
+  public function showAction(Event $event)
+  {
+    $email = new Email();
+    $form = $this->createFormBuilder($email)
+    ->add('email', TextType::class)
+    ->add('submit', SubmitType::class, array('label' => 'Add attendee'))
+    ->getForm();
 
-            $owner = $this->getUser();
-            $event->setOwner($owner);
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($event);
-            $em->flush();
-
-            return $this->redirectToRoute('perso');
-        }
-
-        return $this->render('event/new.html.twig', array(
-            'event' => $event,
-            'form' => $form->createView(),
-        ));
+    if ($form->isSubmitted()) {
+      return $this->redirectToRoute('perso');
     }
 
-    /**
-     * Finds and displays a Event entity.
-     *
-     * @Route("/{id}", name="event_show")
-     * @Method("GET")
-     */
-    public function showAction(Event $event)
-    {
-        return $this->render('event/show.html.twig', array(
-            'event' => $event
-        ));
+    return $this->render('event/show.html.twig', array(
+      'event' => $event,
+      'form' => $form->createView()
+    ));
+  }
+
+  /**
+  * Displays a form to edit an existing Event entity.
+  *
+  * @Route("/{id}/edit", name="event_edit")
+  * @Method({"GET", "POST"})
+  */
+  public function editAction(Request $request, Event $event)
+  {
+    $editForm = $this->createForm('AppBundle\Form\EventType', $event);
+    $editForm->handleRequest($request);
+
+    if ($editForm->isSubmitted() && $editForm->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($event);
+      $em->flush();
+
+      return $this->redirectToRoute('perso');
     }
 
-    /**
-     * Displays a form to edit an existing Event entity.
-     *
-     * @Route("/{id}/edit", name="event_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Event $event)
-    {
-        $editForm = $this->createForm('AppBundle\Form\EventType', $event);
-        $editForm->handleRequest($request);
+    return $this->render('event/edit.html.twig', array(
+      'event' => $event,
+      'edit_form' => $editForm->createView()
+    ));
+  }
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($event);
-            $em->flush();
+  /**
+  * Send invitation
+  *
+  * @Route("/{id}/add", name="event_add")
+  */
+  public function add2Action($id)
+  {
+    $test = $this->get('request')->request->query->get('email');
+    var_dump($test);
+    exit();
+    $email = 'maxime@gmail.com';
+    $this->get('session')->getFlashBag()->add('info', 'invitation to "' . $email . '" has been sent');
+    return $this->redirect($this->generateUrl('event_show', array('id' => $id)));
 
-            return $this->redirectToRoute('perso');
-        }
-
-        return $this->render('event/edit.html.twig', array(
-            'event' => $event,
-            'edit_form' => $editForm->createView()
-        ));
-    }
+  }
 }
